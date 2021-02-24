13efac83688b0043e6bd8ac7c60a341cd57eb8f2
hajo70 <hessmann@spricom.de>
2021-02-24 22:14:11 +0100

    Adds sample for duplicates

---
+++ dessert-sample/src/test/java/de/spricom/dessert/sample/DuplicatesTest.java
@@ -0,0 +1,27 @@
+package de.spricom.dessert.sample;
+
+import de.spricom.dessert.slicing.Classpath;
+import de.spricom.dessert.slicing.ConcreteSlice;
+import de.spricom.dessert.slicing.Slice;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DuplicatesTest {
+
+    @Test
+    void checkDuplicates() {
+        // tag::duplicates[]
+        Classpath cp = new Classpath();
+        ConcreteSlice duplicates = cp.duplicates();
+        duplicates.getClazzes().forEach(clazz -> System.out.println(clazz.getURI()));
+        Assertions.assertThat(duplicates.getClazzes()).isNotEmpty();
+
+        Slice slice = duplicates.minus(cp.asClazz("module-info").getAlternatives());
+        Assertions.assertThat(slice.getClazzes()).isEmpty();
+
+        Slice slice2 = duplicates.minus(cp.slice("module-info"));
+        Assertions.assertThat(slice2.getClazzes()).isEmpty();
+        // end::duplicates[]
+    }
+
+}
