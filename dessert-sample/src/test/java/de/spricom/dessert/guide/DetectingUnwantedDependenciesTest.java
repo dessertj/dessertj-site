3c148f47d3db268f6269e6c0f6113b55aac0c604
hajo70 <hessmann@spricom.de>
2022-06-04 15:23:19 +0200

    Adds motivation section to practical guide

--- dessert-sample/src/test/java/de/spricom/dessert/guide/DetectingUnwantedDependenciesTest.java
+++ dessert-sample/src/test/java/de/spricom/dessert/guide/DetectingUnwantedDependenciesTest.java
@@ -1,8 +1,14 @@
 package de.spricom.dessert.guide;
 
 import de.spricom.dessert.classfile.ClassFile;
+import de.spricom.dessert.partitioning.ClazzPredicates;
 import de.spricom.dessert.slicing.*;
+import de.spricom.dessert.util.AnnotationPattern;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 import static de.spricom.dessert.assertions.SliceAssertions.assertThatSlice;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,14 +35,27 @@ public class DetectingUnwantedDependenciesTest {
     @Test
     void slices() {
         // tag::slices[]
-        // The .jar file or the classes directory containing ClazzResolver.class
+        // All classes of a .jar file or of the classes directory containing ClazzResolver.class
         Root something1 = cp.rootOf(ClazzResolver.class);
 
         // A single class
         Clazz something2 = cp.asClazz(ClazzResolver.class);
 
+        // All classes within a certain package
+        Slice something3 = cp.packageOf(ClazzResolver.class);
 
+        // All classes within a certain package or any sub-package
+        Slice something4 = cp.packageTreeOf(ClazzResolver.class);
+
+        // The package-tree limited to the classes from the something1 Root
+        Slice something5 = something1.packageTreeOf(ClazzResolver.class);
         // end::slices[]
+
+        int index = 1;
+        for (Slice slice : Arrays.asList(something1, something2, something3, something4, something5)) {
+            assertThat(slice.getClazzes()).as("something" + index).isNotEmpty();
+            index++;
+        }
     }
 
     @Test
@@ -47,7 +66,35 @@ public class DetectingUnwantedDependenciesTest {
 
         // An arbitrary list of classes
         Slice unwanted2 = cp.sliceOf(ClazzResolver.class, ClassFile.class);
+
+        // A combination of packages
+        Slice unwanted3 = cp.slice("java.lang.reflect|runtime..*");
+
+        // Classes from internal packages
+        Slice unwanted4 = cp.slice("..internal..*");
+
+        // Classes following some naming pattern
+        Slice unwanted5 = cp.slice("..springframework..*Impl");
+
+        // Everything form a framework but a certain class
+        Slice unwanted6 = cp.slice("..springframework..*").minus(cp.sliceOf(Environment.class));
+
+        // The deprecated classes from the JUnit-Jupiter API .jar
+        Slice unwanted7 = cp.rootOf(Test.class).slice(ClazzPredicates.DEPRECATED);
+
+        // All classes annotated with @Configuration
+        Slice unwanted8 = cp.slice(ClazzPredicates.matchesAnnotation(AnnotationPattern.of(Configuration.class)));
+
+        // The union of two slices
+        Slice unwanted9 = unwanted1.plus(unwanted4);
         // end::unwanted[]
+
+        int index = 1;
+        for (Slice slice : Arrays.asList(unwanted1, unwanted2, unwanted3, unwanted4, unwanted5, unwanted6,
+                unwanted7, unwanted8, unwanted9)) {
+            assertThat(slice.getClazzes()).as("unwanted" + index).isNotEmpty();
+            index++;
+        }
     }
 
 }
